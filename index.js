b818aa45b6ce4de6830e88dd97b32e983315faaa
ERossDGit <ross@simpleinsight.net>
2019-07-17 15:10:25 -0400

    add GET requests for home page and top ten movies

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+
+let topTenMovies = [
+  {
+    title: "Moonstruck",
+    director: "Norman Jewison"
+  },
+  {
+    title: "North by Northwest",
+    director: "Alfred Hitchcock"
+  },
+  {
+    title: "Rear Window",
+    director: "Alfred Hitchcock"
+  },
+  {
+    title: "Searching for Bobby Fischer",
+    director: "Steve Zaillian"
+  },
+  {
+    title: "Sneakers",
+    director: "Phil Alden Robinson"
+  },
+  {
+    title: "The Dark Knight",
+    director: "Christopher Nolan"
+  },
+  {
+    title: "The Bridge on the River Kwai",
+    director: "David Lean"
+  },
+  {
+    title: "Casablanca",
+    director: "Michael Curtiz"
+  },
+  {
+    title: "The Matrix",
+    director: "Lana & Lilly Wachowski"
+  },
+  {
+    title: "Open Range",
+    director: "Kevin Costner"
+  }
+];
+
+// GET requests
+app.get("/", function(req, res) {
+  res.send("Welcome to my movie app!");
+});
+// app.get("/documentation", function(req, res) {
+//   res.sendFile("public/documentation.html", { root: __dirname });
+// });
+app.get("/movies", function(req, res) {
+  res.json(topTenMovies);
+});
+
+// listen for requests
+app.listen(8080, () => console.log("My movie app is listening on port 8080."));
