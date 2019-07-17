cb5b1fbb915cad45b4164373d3268ddec13b469f
ERossDGit <ross@simpleinsight.net>
2019-07-17 15:55:11 -0400

    add morgan middleware to log requests and add error handling

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 let topTenMovies = [
@@ -45,13 +46,15 @@ let topTenMovies = [
 ];
 
 // GET requests
+app.use(morgan("common"));
 app.use(express.static("public"));
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("There has been an error.");
+});
 app.get("/", function(req, res) {
   res.send("Welcome to my movie app!");
 });
-// app.get("/documentation", function(req, res) {
-//   res.sendFile("public/documentation.html", { root: __dirname });
-// });
 app.get("/movies", function(req, res) {
   res.json(topTenMovies);
 });
