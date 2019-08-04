ea2f349ef13550a4dff051d09d2fabbef05022b6
ERossDGit <ross@simpleinsight.net>
2019-08-04 16:06:56 -0400

    add CORS and password hashing

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ const Users = Models.User;
 const passport = require("passport");
 require("./passport");
 
+// Setup CORS
+const cors = require("cors");
+var allowedOrigins = ["http://localhost:8080"];
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        //specific origin isn't found on list of allowed allowedOrigins
+        var message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
+
 mongoose.connect(
   "mongodb://localhost:27017/myFlixDB",
   { useNewUrlParser: true }
@@ -101,10 +120,8 @@ app.get(
  Email : String,
  Birthday : Date
 }*/
-app.post("/users", passport.authenticate("jwt", { session: false }), function(
-  req,
-  res
-) {
+app.post("/users", function(req, res) {
+  var hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then(function(user) {
       if (user) {
@@ -112,7 +129,7 @@ app.post("/users", passport.authenticate("jwt", { session: false }), function(
       } else {
         Users.create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday
         })
